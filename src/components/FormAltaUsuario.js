9b8004ca8acfb2b3f9b2587039dbf6934f4d07af
MaxiRuiz <maxiruiz@gmail.com>
2025-03-12 00:19:05 -0300

    Se dio formato a formulario alta usuario

--- src/components/FormAltaUsuario.js
+++ src/components/FormAltaUsuario.js
@@ -1,78 +1,99 @@
 
 // FormAltaUsuario.js
 import React from 'react';
-import { CFormInput, CForm, CCol, CRow, CFormSelect, CButton } from '@coreui/react';
+import { CFormInput, CForm, CCol, CRow, CFormSelect, CButton, CContainer, CCard, CCardBody, CCardFooter } from '@coreui/react';
+import { CInputGroup, CInputGroupText,CDropdown,CDropdownMenu, CDropdownItem, CDropdownToggle,} from '@coreui/react';
 
 
 
 const FormAltaUsuario = ({ id, label, placeholder }) => {
+
+  const guardar = () => {
+  //Leemos los datos de data/datos.json
+    useEffect(() => {
+      // Realizar la solicitud fetch
+      fetch('../../../data/datos.json')
+        .then(response => response.json())  // Convertimos la respuesta a formato JSON
+        .then(data => {
+          setTableData(data);  // Actualizamos el estado con los datos cargados
+          })
+        .catch(error => {
+          console.error('Error al cargar los datos:', error);
+          });
+        }
   
   
+
+
+
+
+
   return (
-    <>
-      
+    <CContainer>
+      <CCard className="mb-4">
       <CForm className="row g-3">
-        
-        <CCol md={6}>
-                <CFormInput type="text" id="inputApellido" label="Apellido" />
-        </CCol>
+        <CCardBody className="p-3" >
+      
 
-        <CCol md={6}>
-                <CFormInput type="text" id="inputNombre" label="Nombre" />
-        </CCol>
+      <c-input-group className="shadow-sm border-0 mb-0 size=sm">
 
-        <CRow className="mt-4">
-          <CCol md={6}>
-            <CFormInput type="text" id="inputDomicilio" label="Domicilio" />
-          </CCol>
-        </CRow>
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputNombre">Apellido y Nombre</CInputGroupText>
+          <CFormInput placeholder="" aria-label="Nombre" aria-describedby="basic-addon1" />
+        </CInputGroup>
 
-        <CCol md={6}>
-          <CFormSelect
-            id="inputProvincia" 
-            label="Provincia" 
-            aria-label="Selec una provincia"
-            options={[
-              { label: 'Seleccione una provincia' },
-              { label: 'One', value: '1' },
-              { label: 'Two', value: '2' },
-              { label: 'Three', value: '3'},
-            ]}
-          />        
-        </CCol>
-                
-        <CCol md={6}>
-          <CFormSelect
-            id="inputLocalidad" 
-            label="Localidad" 
-            aria-label="Selec localidad"
-            options={[
-              { label: 'Seleccione localidad' },
-              { label: 'One', value: '1' },
-              { label: 'Two', value: '2' },
-              { label: 'Three', value: '3'},
-            ]}
-          />
-        </CCol>
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputDomicilio">Domicilio</CInputGroupText>
+          <CFormInput placeholder="" aria-label="Domicilio" aria-describedby="basic-addon1" />
+        </CInputGroup>
 
-        <CCol md={4}>
-                <CFormInput type="number" id="inputDNI" label="DNI" />
-        </CCol>
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputProvincia" aria-label="Provincia" >Provincia</CInputGroupText>
+          <CFormSelect id="inputGroupSelect01" >
+              <option> Seleccione una provincia</option>
+              <option value='1'>Entre Ríos</option>
+              <option value='2'>Santa Fé</option>
+              <option value='3'>Mendoza</option>
+          </CFormSelect>
+        </CInputGroup>
 
-        <CCol md={4}>
-                <CFormInput type="date" id="inputFecNac" label="Fecha Nacimiento" />
-        </CCol>
-        
-        <div>
-        <CCol md={4}>
-                <CFormInput type="email" id="Email" label="Email" />
-        </CCol>
-        </div>
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputLocalidad" aria-label="Localidad" >Localidad</CInputGroupText>
+          <CFormSelect id="inputGroupSelect01" >
+              <option> Seleccione una localidad</option>
+              <option value='1'>Paraná</option>
+              <option value='2'>Nogoyá</option>
+              <option value='3'>Viale</option>
+          </CFormSelect>
+        </CInputGroup>
 
-        
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputDNI">Documento de Identidad</CInputGroupText>
+          <CFormInput placeholder="" aria-label="DNI" aria-describedby="basic-addon1" />
+        </CInputGroup>
+
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputFecNac">Fecha de Nacimiento</CInputGroupText>
+          <CFormInput placeholder="" aria-label="Fecha de nacimiento" type="date" aria-describedby="basic-addon1" />
+        </CInputGroup>
+
+        <CInputGroup className="mb-3">
+          <CInputGroupText id="inputEmail">Email</CInputGroupText>
+          <CFormInput placeholder="" aria-label="Email" type="email" aria-describedby="basic-addon1" />
+        </CInputGroup>
+
+
+        </c-input-group>
+      </CCardBody>
+      </CForm>
+
+
+      <CCardFooter className="px-4 py-0 bg-light"> {/* Fondo claro  */}
+
+
         <CRow className="mb-3">
           <CCol>
-            <div className="d-flex justify-content-start">
+            <div className="d-flex justify-content-end">
           
               <CButton className="text-white me-2 ms-2 mt-3 mb-3"  color="success" type="submit">
                 Guardar
@@ -90,20 +111,21 @@ const FormAltaUsuario = ({ id, label, placeholder }) => {
          
           </CCol>
         </CRow>
+      </CCardFooter>
 
 
 
 
       
       
-      </CForm>
+      
       
 
 
 
-    
-
-    </>
+      
+      </CCard>
+    </CContainer>
   );
 };
 
